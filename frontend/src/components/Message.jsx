bdbcb721e2d148b363a623643a2ae307797d0d6e
Daksh Pal <pal.daksh22@gmail.com>
2024-07-18 21:17:23 +0530

    Used redux to remove the selected User from store

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -10,16 +10,17 @@ const Message = ({message}) => {
     scroll.current?.scrollIntoView({behavior:"smooth"});
   },[message])
 
-  return (
-        <div ref={scroll} className={`mt-4 chat ${authUser?._id === message?.senderId ? 'chat-end' : 'chat-start' }`}>
-            <div className="chat-bubble bg-violet-700 text-white">{message?.message}</div>
-            <div className='w-10 rounded-full'>
-              <img src={message.senderId===authUser?._id ? authUser?.profilePhoto : selectedUser?.profilePhoto } alt="Profile photo" />
-            </div>
-            <div>
-              <time className='text-xs opacity-50 text-black'> 12:45 </time>
-            </div>
-        </div>
+
+    return (
+      <div ref={scroll} className={`mt-4 chat ${authUser?._id === message?.senderId ? 'chat-end' : 'chat-start' }`}>
+          <div className="chat-bubble bg-violet-700 text-white">{message?.message}</div>
+          <div className='w-10 rounded-full'>
+            <img src={message.senderId===authUser?._id ? authUser?.profilePhoto : selectedUser?.profilePhoto } alt="" />
+          </div>
+          <div>
+            <time className='text-xs opacity-50 text-black'> 12:45 </time>
+          </div>
+      </div>
   )
 }
 
--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -1,31 +1,48 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import SendInput from './SendInput'
 import Messages from './Messages';
-import proPic from "./proPic.jpg";
-import { useSelector } from 'react-redux';
+import { setSelectedUser , setAuthUser  } from "../redux/userSlice";
+import { useDispatch, useSelector } from 'react-redux';
 
 const MessageContainer = () => {
 
-    const {selectedUser} = useSelector(store => store.user);
+    const {selectedUser , authUser} = useSelector(store => store.user);
+    console.log(authUser);
+    const dispatch = useDispatch()
 
+    useEffect(()=>{
+        return()=> dispatch(setSelectedUser(null))
+    },[])
 
-  return (
-      <div className='md:min-w-[550px] flex flex-col'>
-        <div className='flex gap-2 items-center bg-violet-500 rounded-sm text-white px-4 py-2 cursor-pointer '>
-            <div className='avatar online'>
-                <div className='w-12 rounded-full'>
-                    <img src={selectedUser?.profilePhoto} alt="Pro-Pic" />
-                    </div>
-                </div>
-                <div className='flex'>
-                    <div>
-                        <p>{selectedUser?.fullName}</p>
-                    </div>
-                </div>
+    if(!selectedUser){
+        return(
+            <div className='md:min-w-[550px] flex flex-col items-center justify-center'>
+                <h1 className='text-violet-800 font-bold text-2xl shadow-xl'>Hi, {authUser?.fullName} </h1>
+                <h1 className='text-violet-800 font-bold text-2xl shadow-xl'>Select an User to start Conversation</h1>
             </div>
-            <Messages/>
-            <SendInput/>
-    </div>
-  )
+        )
+    }
+    else{
+        return (
+            <div className='md:min-w-[550px] flex flex-col'>
+              <div className='flex gap-2 items-center bg-violet-500 rounded-sm text-white px-4 py-2 cursor-pointer'>
+                  <div className='avatar online'>
+                      <div className='w-12 rounded-full'>
+                          <img src={selectedUser?.profilePhoto} alt="Pro-Pic" />
+                          </div>
+                      </div>
+                      <div className='flex'>
+                          <div>
+                              <p>{selectedUser?.fullName}</p>
+                          </div>
+                      </div>
+                  </div>
+                  <Messages/>
+                  <SendInput/>
+          </div>
+        )
+    }
+
+  
 }
 export default MessageContainer
